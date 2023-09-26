99ca5f5553ce288d74529cfbf7f7154502d4dbe1
Az1tchhh <azamatbazarbai007@gmail.com>
2023-09-26 19:25:22 +0600

    back created on spring mvn

---
+++ Backend/Coffee-Berw-back/src/main/java/kz/coffeeberw/coffeeberwback/CoffeeBerwBackApplication.java
@@ -0,0 +1,13 @@
+package kz.coffeeberw.coffeeberwback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBerwBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBerwBackApplication.class, args);
+	}
+
+}

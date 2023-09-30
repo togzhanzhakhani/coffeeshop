59b91373cba5cd4ed614e4b78908952b7e4cadfc
Az1tchhh <azamatbazarbai007@gmail.com>
2023-09-30 12:40:41 +0600

    print

--- Backend_py/api/views.py
+++ Backend_py/api/views.py
@@ -32,7 +32,15 @@ def list_of_sorts(request):
         return Response(serializer.data)
 
 
+@api_view(['GET'])
+def list_of_commentaries_of_a_product(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist as e:
+        return Response({"error" : str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-
-
+    if request.method == 'GET':
+        commentaries =product.comments.all()
+        serializer = CommentSerializer(commentaries, many=True)
+        return Response(serializer.data)
 

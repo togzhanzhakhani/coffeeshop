79fa803cf9f0022687a5bfa075d48efb128b35f8
Az1tchhh <azamatbazarbai007@gmail.com>
2023-09-30 12:31:15 +0600

    created components and services
    need to fill services first to check for its consumable

--- Frontend/Coffee-Berw/src/app/app.module.ts
+++ Frontend/Coffee-Berw/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { BasketComponent } from './basket/basket.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    AuthenticationComponent,
+    BasketComponent,
+    CategoryComponent,
+    ProductDetailComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,

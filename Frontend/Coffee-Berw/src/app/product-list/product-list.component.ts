f9f709aa779087d44d8c96beb11b75b832b4da7e
Az1tchhh <azamatbazarbai007@gmail.com>
2023-10-02 18:37:07 +0600

    top=bar and product-list components works fine...

--- Frontend/Coffee-Berw/src/app/product-list/product-list.component.ts
+++ Frontend/Coffee-Berw/src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../product.service";
 import {Product} from "../models/Product";
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-product-list',
@@ -8,17 +9,20 @@ import {Product} from "../models/Product";
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit{
-  productsFound!: Product[];
+  currentProducts!: Product[];
+
+  isLogged = AppComponent.isLogged;
   constructor(private productService: ProductService) {
   }
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe((data)=>{
       console.log("init products", data);
+      this.currentProducts = data;
     })
   }
 
   onProductsFound(products: Product[]){
-    this.productsFound = products;
-    console.log("products in product component", this.productsFound);
+    this.currentProducts = products;
+    console.log("products in product component", this.currentProducts);
   }
 }
